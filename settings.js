dafff167b25390e0fc9ab0da175d84063dfa4cff
VIP-WABUG <114746072+VIP-WABUG@users.noreply.github.com>
2023-01-02 10:37:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Callmefii___' 
+global.apikey = ["Callmefii___", "Alphabot", "Alpha"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
